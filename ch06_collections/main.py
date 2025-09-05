762a68e19d6a3a45f2e6a3129707d21b7608c696
maybeags <suren0709@gmail.com>
2025-09-05 16:42:53 +0900

    feat: constructor lesson

--- ch06_collections/main.py
+++ ch06_collections/main.py
@@ -391,17 +391,128 @@ elif month_int in [ 1, 3, 5, 7, 8, 10, 12 ]:
 
 입력 받은 연락처는 {'김일':'010-1234-5678', '김이':'010-2345-6789', '김삼':'010-3456-7890'}입니다.
 '''
-telephone = {}
-num_of_people = 3
-for i in range(num_of_people):
-    dict_key = input(f'{i+1} 번 째 사람의 이름을 입력하세요 >>> ')
-    dict_value = input(f'{i+1} 번 째 사람의 연락처를 입력하세요 >>> ')
-    #딕셔너리에 value 대입하는 방법
-    telephone[dict_key] = dict_value
-
-print(f'입력 받은 연락처는 {telephone}입니다.')
+# telephone = {}
+# num_of_people = 3
+# for i in range(num_of_people):
+#     dict_key = input(f'{i+1} 번 째 사람의 이름을 입력하세요 >>> ')
+#     dict_value = input(f'{i+1} 번 째 사람의 연락처를 입력하세요 >>> ')
+#     #딕셔너리에 value 대입하는 방법
+#     telephone[dict_key] = dict_value
+#
+# print(f'입력 받은 연락처는 {telephone}입니다.')
 
 '''
 식사 후에는 collections + function
 
+숫자를 입력한 횟수만큼 비어있는 list에 숫자를 추가하기
+문제 : 비어있는 numbers1을 선언하고, 그 안에 입력 받은 횟수만큼 숫자를 추가하시오.
+
+함수 정의 : add_numbers()
+매개 변수 : 정수 n
+
+함수 호출
+add_numbers1(last_num)          # call2() 유형
+print(add_numbers2(last_num))   # call4() 유형
+
+실행 예
+숫자 몇 까지 입력하시겠습니까? >>> 10
+[1,2,3,4,5,6,7,8,9,10]
+[1,2,3,4,5,6,7,8,9,10]
+
+예를 들어 hello = ['안', '녕', '하', '세', '요']라는 list가 있다고 가정했을 때,
+add_numbers3(10, hello)를 호출하면
+[1,2,3,4,5,6,7,8,9,10,'안','녕','하','세','요']
+라는 결과값을 만드는 함수를 정의한다면 어떻게 할 수 있을지 고민해보세요.
 '''
+# numbers1 = []
+# 함수 정의 영역
+# def add_numbers1(n):
+#     for i in range(n):
+#         numbers1.append(i+1)
+#     print(numbers1)
+
+# 함수 호출 영역
+# last_num = int(input('숫자 몇 까지 입력하시겠습니까? >>> '))
+# add_numbers1(last_num)
+#
+# numbers2 = []
+# # add_numbers2 함수 정의 영역
+# def add_numbers2(n):
+#     #pass    # 이거 쓰면 밑에 함수 호출하거나 구현부를 작성하지 않아도 오류가 나지 않습니다.
+#     for i in range(n):
+#         numbers2.append(i+1)
+#     return numbers2
+# add_numbers2 함수 호출 영역
+# print(add_numbers2(last_num))
+
+
+# 함수 정의 영역
+# def add_numbers3(n, temp_list):
+    # # 1 다수의 수강생분들이 생각해낸 방식
+    # temp_temp_list =[]
+    # for i in range(n):
+    #     temp_temp_list.append(i+1)
+    # result = temp_temp_list + temp_list
+    # print(result)
+
+    # # 2 강사가 처음에 생각한 방식
+    # for i in range(n):
+    #     temp_list.insert(i, i+1)
+    # print(temp_list)
+
+# 함수 호출 영역
+# hello = ['안', '녕', '하', '세', '요']
+# add_numbers3(last_num, hello)  # 결과값 : [1,2,3,4,5,6,7,8,9,10,'안','녕','하','세','요']
+#insert 활용 방식으로도 풀어보세요
+'''
+짝수와 홀수의 개수 세기
+list를 입력 받아 짝수와 홀수의 개수를 세는 함수를 작성하시오.
+
+함수 정의
+함수 이름 : count_even_odd
+매개변수 : list인 numbers(요소는 모두 정수일 것)
+
+함수 호출
+count_even_odd([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+실행 예
+짝수의 개수 : 5개
+홀수의 개수 : 5개
+'''
+def count_even_odd1(numbers):
+    even_count = 0
+    odd_count = 0
+    for number in numbers:
+        if number % 2 == 0:
+            even_count += 1
+        else:
+            odd_count += 1
+    print(f'짝수의 개수 : {even_count}')
+    print(f'홀수의 개수 : {odd_count}')
+
+count_even_odd1([1,2,3,4,5,6,7,8,9,10])
+
+def count_even_odd2(numbers):
+    evens = []
+    odds = []
+    for number in numbers:
+        if number % 2 == 0:
+            evens.append(number)
+        else:
+            odds.append(number)
+    print(f'짝수의 개수 : {len(evens)}')
+    print(f'홀수의 개수 : {len(odds)}')
+
+count_even_odd2([1,2,3,4,5,6,7,8,9,10])
+
+def count_even_odd3(numbers):
+    evens = []
+    for number in numbers:
+        if number % 2 == 0:
+            evens.append(number)
+    print(f'짝수의 개수 : {len(evens)}')
+    print(f'홀수의 개수 : {len(numbers) - len(evens)}')
+
+count_even_odd3([1,2,3,4,5,6,7,8,9,10])
+
+# ch07_classes -> main
