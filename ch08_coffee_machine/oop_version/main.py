32b2819c50c9bf65627494879cf4e34d702f61af
maybeags <suren0709@gmail.com>
2025-09-10 17:46:29 +0900

    feat: 20250910 oop lesson

--- ch08_coffee_machine/oop_version/main.py
+++ ch08_coffee_machine/oop_version/main.py
@@ -19,10 +19,20 @@ while is_on:
         coffee_maker.report()
         money_machine.report()
     else:
+        # if choice not in ['espresso', 'cappuccino', 'latte']:   # 얘는 메뉴 추가될 때마다 수정해야해서 좀...
+        #     continue
+
         drink = menu.find_drink(choice)
+        if drink == None:               # choice에 오타가 있을 경우 None이 반환되기 때문에 고전적 예외 처리
+            continue                    # continue가 실행되면 이 이하의 코드라인은 실행되지 않고,
+                                        # while 반복문의 가장 앞 부분으로 돌아가서 다음 반복이 실행됨.
+
         # 음료 이름을 입력 받은 시점부터의 프로세스를 떠올려서 코드를 작성하셔야만 합니다. 이때 고려해야할 것은 절차지향 방식으로 코딩했을 때의 과정과, 현재 참조해야 하는 파이썬 모듈들의 매개변수 차이와 메서드 작동 원리의 차이를 감안해서 코드를 쓸 필요가 있다는 점입니다.
         # pop version의 함수 작성 순서대로 저희가 작동하도록 했습니다.
         # is_resources_enough(order_ingredient) / is_resource_sufficient(drink)
         if coffee_maker.is_resource_sufficient(drink):
             # 조건문을 이용해 process_coins()와 make_payment()를 활용하여,
             # 모든 것이 true일 때 '작동 중입니다'를 출력할 수 있도록 작성하시오.
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
+
